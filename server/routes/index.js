12be0f91e3bd019d69418a029aa3f161b2a0b5c9
HelloMandu <tjdals6695@gmail.com>
2021-01-02 20:22:26 +0900

    challenge route 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const user = require("./user");
+const challenge = require("./challenge");
 
 router.use("/user", user);
+router.use("/challenge", challenge);
 
 module.exports = router;
